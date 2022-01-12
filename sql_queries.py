6affd6dd79b6236365fa5d78182e37f568419d0f
unknown <jblackman.prof@gmail.com>
2022-01-12 10:27:57 -0500

    adjusted sql formatting to be more readable

--- sql_queries.py
+++ sql_queries.py
@@ -19,48 +19,93 @@ DROP TABLE IF EXISTS time;
 # CREATE TABLES
 
 songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS song_play (songplay_id SERIAL PRIMARY KEY, start_time bigint, user_id int, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar,
-CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES app_user(user_id),
-CONSTRAINT fk_song FOREIGN KEY(song_id) REFERENCES song(song_id),
-CONSTRAINT fk_artist FOREIGN KEY(artist_id) REFERENCES artist(artist_id));
+CREATE TABLE IF NOT EXISTS song_play (
+    songplay_id SERIAL  PRIMARY KEY, 
+    start_time  bigint, 
+    user_id     int, 
+    level       varchar, 
+    song_id     varchar, 
+    artist_id   varchar, 
+    session_id  int, 
+    location    varchar, 
+    user_agent  varchar,
+    CONSTRAINT fk_user 
+        FOREIGN KEY(user_id) 
+            REFERENCES app_user(user_id),
+    CONSTRAINT fk_song 
+        FOREIGN KEY(song_id) 
+            REFERENCES song(song_id),
+    CONSTRAINT fk_artist 
+        FOREIGN KEY(artist_id) 
+            REFERENCES artist(artist_id));
 """)
 
 user_table_create = ("""
-CREATE TABLE IF NOT EXISTS app_user (user_id int PRIMARY KEY, first_name varchar, last_name varchar, gender char, level varchar)
+CREATE TABLE IF NOT EXISTS app_user (
+    user_id     INT     PRIMARY KEY, 
+    first_name  varchar, 
+    last_name   varchar, 
+    gender      char, 
+    level       varchar)
 """)
 
 song_table_create = ("""
-CREATE TABLE IF NOT EXISTS song (song_id varchar PRIMARY KEY, title varchar, artist_id varchar, year int, duration numeric)
+CREATE TABLE IF NOT EXISTS song (
+    song_id     varchar     PRIMARY KEY, 
+    title       varchar, 
+    artist_id   varchar, 
+    year        int, 
+    duration    numeric)
 """)
 
 artist_table_create = ("""
-CREATE TABLE IF NOT EXISTS artist (artist_id varchar PRIMARY KEY, name varchar, location varchar, latitude numeric, longitude numeric)
+CREATE TABLE IF NOT EXISTS artist (
+    artist_id   varchar     PRIMARY KEY, 
+    name        varchar, 
+    location    varchar, 
+    latitude    numeric, 
+    longitude   numeric)
 """)
 
 time_table_create = ("""
-CREATE TABLE IF NOT EXISTS time (start_time bigint, hour int, day int, week int, month varchar, year int, weekday int)
+CREATE TABLE IF NOT EXISTS time (
+    start_time  bigint, 
+    hour        int, 
+    day         int, 
+    week        int, 
+    month       varchar, 
+    year        int, 
+    weekday     int)
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-INSERT INTO song_play (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+INSERT INTO song_play (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
 user_table_insert = ("""
-INSERT INTO app_user (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING;
+INSERT INTO app_user (user_id, first_name, last_name, gender, level) 
+VALUES (%s, %s, %s, %s, %s) 
+ON CONFLICT DO NOTHING;
 """)
 
 song_table_insert = ("""
-INSERT INTO song (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING;
+INSERT INTO song (song_id, title, artist_id, year, duration) 
+VALUES (%s, %s, %s, %s, %s) 
+ON CONFLICT DO NOTHING;
 """)
 
 artist_table_insert = ("""
-INSERT INTO artist (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING;
+INSERT INTO artist (artist_id, name, location, latitude, longitude) 
+VALUES (%s, %s, %s, %s, %s) 
+ON CONFLICT DO NOTHING;
 """)
 
 time_table_insert = ("""
-INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s);
+INSERT INTO time (start_time, hour, day, week, month, year, weekday) 
+VALUES (%s, %s, %s, %s, %s, %s, %s);
 """)
 
 # FIND SONGS
